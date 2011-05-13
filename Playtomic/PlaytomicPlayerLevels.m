f678a9af2bab0e9a7a9cc668dc73969f527fd9d9
Playtomic <ben@playtomic.com>
2011-05-13 12:02:14 -0600

    added copyright etc

--- Playtomic/PlaytomicPlayerLevels.m
+++ Playtomic/PlaytomicPlayerLevels.m
@@ -2,9 +2,37 @@
 //  PlaytomicPlayerLevels.m
 //  ObjectiveCTest
 //
-//  Created by ben@playtomic.com on 2/25/11.
+//  This file is part of the official Playtomic API for iOS games.  
+//  Playtomic is a real time analytics platform for casual games 
+//  and services that go in casual games.  If you haven't used it 
+//  before check it out:
+//  http://playtomic.com/
+//
+//  Created by ben at the above domain on 2/25/11.
 //  Copyright 2011 Playtomic LLC. All rights reserved.
 //
+//  Documentation is available at:
+//  http://playtomic.com/api/ios
+//
+// PLEASE NOTE:
+// You may modify this SDK if you wish but be kind to our servers.  Be
+// careful about modifying the analytics stuff as it may give you 
+// borked reports.
+//
+// If you make any awesome improvements feel free to let us know!
+//
+// -------------------------------------------------------------------------
+// THIS SOFTWARE IS PROVIDED BY PLAYTOMIC, LLC "AS IS" AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #import "PlaytomicLevel.h"
 #import "PlaytomicResponse.h"
@@ -57,6 +85,7 @@
 
     NSString* playerid = [level valueForKey: @"PlayerId"];
     NSString* playername = [[level valueForKey: @"PlayerName"] stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+    NSString* playersource = [level valueForKey: @"PlayerSource"];
     NSString* name = [[level valueForKey: @"Name"] stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
     NSString* ldata = [[level valueForKey: @"Data"] stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
     NSString* lthumb = [level valueForKey: @"Thumb"];
@@ -76,7 +105,7 @@
         [customdata setObject: cvalue forKey: key];
     }
     
-    [md addObject: [[PlaytomicLevel alloc] initWithName:name andPlayerName:playername andPlayerId:playerid andData:ldata andThumb: lthumb andVotes:votes andPlays:plays andRating:rating andScore:score andDate:date andRelativeDate:relativedate andCustomData:customdata andLevelId: levelid]];
+    [md addObject: [[PlaytomicLevel alloc] initWithName:name andPlayerName:playername andPlayerId:playerid andPlayerSource: playersource andData:ldata andThumb: lthumb andVotes:votes andPlays:plays andRating:rating andScore:score andDate:date andRelativeDate:relativedate andCustomData:customdata andLevelId: levelid]];
 
     return [[PlaytomicResponse alloc] initWithSuccess: true andErrorCode: 0 andData: md andNumResults: 1];
 }
